687ad072a47913bee85b250ef8434cdde5cee343
banga <dennisbanga@gmail.com>
2015-08-31 15:35:22 +0300

    humans.txt and robots.txt

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import home
+from .views import home, home_files
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'taskbuster.views.home', name='home'),
@@ -8,4 +9,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
 ]
